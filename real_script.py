334a0a8aacd31e39fdfa29ea80ed5f85e7e59c53
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-12-09 12:01:58 +0300

    Update real_script.py

--- real_script.py
+++ real_script.py
@@ -7,7 +7,6 @@ for paragraph in doc.paragraphs:
     if re.search(r"\d{5,6}", paragraph.text) != None:
         result = int(re.search(r"\d{5,6}", paragraph.text).group(0))
         price = result + 3500
-        result_as_str = str(result)
         paragraph.text = re.sub('\d{5,6}', '', paragraph.text)
         paragraph.text += f"{price}"
         print(paragraph.text)

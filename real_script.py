439bf219f2eb23d8215364df2c6d6e3d28d70dd8
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-12-10 18:13:24 +0300

    Update real_script.py

--- real_script.py
+++ real_script.py
@@ -2,11 +2,12 @@ import docx
 import re
 doc = docx.Document("doc.docx")
 
+sum = int(input("How much do you want to add to the prices? "))
 
 for paragraph in doc.paragraphs:
     if re.search(r"\d{5,6}", paragraph.text) != None:
         result = int(re.search(r"\d{5,6}", paragraph.text).group(0))
-        price = result + 3500
+        price = result + sum
         paragraph.text = re.sub('\d{5,6}', '', paragraph.text)
         paragraph.text += f"{price}"
         print(paragraph.text)
